87bcb24c9ac7d74c29820482c0cb81a1f60d4485
Idowu Gabriel <peoplemerit@gmail.com>
2025-09-13 21:35:34 -0400

    Remove DOMContentLoaded event listener and partial loads

--- assets/js/boot-includes.js
+++ assets/js/boot-includes.js
@@ -1,16 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const loadPartial = async (containerId, filePath) => {
-    try {
-      const response = await fetch(filePath);
-      if (!response.ok) throw new Error(`Could not load ${filePath}`);
-      const text = await response.text();
-      document.getElementById(containerId).innerHTML = text;
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
-  loadPartial('header-container', 'partials/header.html');
-  loadPartial('body-container', 'partials/body.html');
-  loadPartial('footer-container', 'partials/footer.html');
-});
